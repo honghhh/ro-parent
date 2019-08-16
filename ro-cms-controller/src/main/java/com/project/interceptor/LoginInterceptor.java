5d0eaa13d74c6e0decc732ec411cf8e52035fc9d
huanghong <350569321@qq.com>
2019-08-16 18:42:56 +0800

    添加配置

---
+++ ro-cms-controller/src/main/java/com/project/interceptor/LoginInterceptor.java
@@ -0,0 +1,10 @@
+package com.project.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * 登录拦截器
+ */
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+}
---
+++ ro-cms-controller/src/main/java/com/project/utils/MappingUtil.java
@@ -0,0 +1,10 @@
+package com.project.utils;
+
+/**
+ * 地址映射工具类
+ */
+public class MappingUtil {
+
+    /** 登录页 */
+    public static final String USER_USER = "/user/user";
+}
