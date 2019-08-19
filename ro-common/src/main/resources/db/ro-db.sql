d393f819696e50eaa4a676ac7dff350c6476e81a
huanghong <350569321@qq.com>
2019-08-19 20:20:21 +0800

    修改表

--- ro-common/src/main/resources/db/ro-db.sql
+++ ro-common/src/main/resources/db/ro-db.sql
@@ -11,11 +11,40 @@
  Target Server Version : 50560
  File Encoding         : 65001
 
- Date: 19/08/2019 18:01:05
+ Date: 19/08/2019 20:19:54
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for menu
+-- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parentid` int(11) NULL DEFAULT NULL COMMENT '父级菜单id',
+  `menuname` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '菜单名称',
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '访问地址',
+  `icon` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单样式标识',
+  `sort` int(11) NULL DEFAULT 0 COMMENT '排序',
+  `status` int(11) NOT NULL DEFAULT 0 COMMENT '0启用 -1禁用',
+  `type` int(2) NULL DEFAULT NULL COMMENT '1显示菜单 2功能菜单',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '菜单表' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '角色名称',
+  `menuids` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '角色菜单权限组，逗号分隔',
+  `status` int(1) NULL DEFAULT 0 COMMENT '0有效 -1无效',
+  `createtime` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for user
@@ -31,11 +60,13 @@ CREATE TABLE `user`  (
   `status` int(2) NULL DEFAULT 0 COMMENT '0有效 -1无效',
   `createtime` datetime NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '后台账号表' ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '后台账号表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', '888888', 'adsfsdf', NULL, NULL, 0, NULL);
+INSERT INTO `user` VALUES (1, 'admin', '21218cca77804d2ba1922c33e0151105', 'adsfsdf', NULL, NULL, 0, NULL);
+INSERT INTO `user` VALUES (2, '18316770399', '21218cca77804d2ba1922c33e0151105', NULL, NULL, NULL, 0, NULL);
+INSERT INTO `user` VALUES (3, '13800138000', '21218cca77804d2ba1922c33e0151105', NULL, NULL, NULL, 0, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
